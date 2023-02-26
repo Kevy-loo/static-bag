4cf4a60eb0d281c9ea3dfeea477cd8c887440328
kevin-liu <liu.kevin1997@gmail.com>
2023-02-26 15:03:46 -0500

    finished

--- Bag.js
+++ Bag.js
@@ -1,8 +1,11 @@
 class Bag {
+    static MAX_WEIGHT = 23;
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
-        } 
+        } else if (weight > Bag.MAX_WEIGHT) {
+            throw new Error(`bag cannot be more than ${MAX_WEIGHT}` )
+        }
         this.weight = weight
     }
 }
--- Bag.test.js
+++ Bag.test.js
@@ -1,13 +1,17 @@
-const Bag = require('./Bag')
+const Bag = require("./Bag");
 
-describe('Bag class', () => {
-    //bag has weight
-    test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
-    })
-    //if bag has no weight, return error
-    test('bag has no weight to return an error', () => {
-        expect(() => new Bag()).toThrowError('bag needs weight')
-    })
-})
+describe("Bag class", () => {
+  //bag has weight
+  test("bag has weight", () => {
+    const bag = new Bag(22);
+    expect(bag.weight).toBe(22);
+  });
+  //if bag has no weight, return error
+  test("bag has no weight to return an error", () => {
+    expect(() => new Bag()).toThrowError("bag needs weight");
+  });
+  test("bag weight is less than the maximum weight", () => {
+    const bag = new Bag(22);
+    expect(bag.weight).toBeLessThan(Bag.MAX_WEIGHT);
+  });
+});
